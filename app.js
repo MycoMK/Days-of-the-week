e17b11148719b7d8279f5008276fb94906567fff
Myco <kmichael831@gmail.com>
2022-09-12 22:08:04 -0700

    functions

---
+++ app.js
@@ -0,0 +1,28 @@
+// function singSong(){
+//     console.log('Do')
+//     console.log('Re')
+//     console.log('DMi')
+// }
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+
+
+// function greet (firstName){
+//     console.log(`Hey there ${firstName}`)
+// }
+   
+function greet(firstName, lastName){
+    console.log(`Hey there ${firstName} ${lastName[0]}`)
+}
+
+function add (x , y){
+    console.log( x + y)
+}
+
+function add(x,y){
+    let sum = (x+y);
+    return sum;
+}
