4da97c68c3d4f951707d4c3bff663507344cdede
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2023-04-19 17:05:20 -0700

    edits

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let btn1 = document.querySelector("btn-1")
+let btn2 = document.querySelector("btn-2")
+let btn3 = document.querySelector("btn-3")
+let btn4 = document.querySelector("btn-4")
+let btn5 = document.querySelector("btn-5")
+
+btn1.addEventListener("click",()=>{
+    document.body.style.backgroundImage = `url("cake -3.jpg")`
+})
+btn2.addEventListener("click",()=>{
+    document.body.style.backgroundImage = `url("fly bird.jpg")`
+})
+btn3.addEventListener("click",()=>{
+    document.body.style.backgroundImage = `url("PAGE 3.jpg")`
+})
+btn4.addEventListener("click",()=>{
+    document.body.style.backgroundImage = `url("cake -3.jpg")`
+})
