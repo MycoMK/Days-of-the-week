b7461cc8c426692e74ba1ff82533a7fcfc73598f
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2023-04-19 17:47:28 -0700

    edits

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 
-let btn1 = document.querySelector("btn-1")
-let btn2 = document.querySelector("btn-2")
-let btn3 = document.querySelector("btn-3")
-let btn4 = document.querySelector("btn-4")
-let btn5 = document.querySelector("btn-5")
+
+let h1 = document.querySelector("h1");
+h1.innerText = "HAWAII-GROWN-CACAO"
+
+
+
+
+let btn1 = document.querySelector("#btn-1")
+let btn2 = document.querySelector("#btn-2")
+let btn3 = document.querySelector("#btn-3")
+let btn4 = document.querySelector("#btn-4")
+let btn5 = document.querySelector("#btn-5")
+
 
 btn1.addEventListener("click",()=>{
-    document.body.style.backgroundImage = `url("cake -3.jpg")`
-})
-btn2.addEventListener("click",()=>{
-    document.body.style.backgroundImage = `url("fly bird.jpg")`
-})
-btn3.addEventListener("click",()=>{
-    document.body.style.backgroundImage = `url("PAGE 3.jpg")`
-})
-btn4.addEventListener("click",()=>{
-    document.body.style.backgroundImage = `url("cake -3.jpg")`
+    document.body.style.backgroundImage = `url("1.jpg")`;
 })
